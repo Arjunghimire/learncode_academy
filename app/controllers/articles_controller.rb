a3bab075f49f76926938cbdd49406e569bfcfbf1
Arjun Ghimire <arjunrazz0714@gmail.com>
2016-04-18 20:39:09 +0545

    Adding Html document and Hello World

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,22 @@
 class ArticlesController < ApplicationController
   def index
   	 @articles=Article.all.order("created_at ASC")
+  	 @html_syntax="<!DOCTYPE html>
+  	 <html>
+  	 <head>
+  	 <meta http-equiv='Content-Type' content='text/html; charset=utf-8'>
+  	 <title>Hello HTML5 World</title>
+  	 </head>
+  	 <body>
+  	 <h1>Hello HTML5</h1>
+  	 <p>Welcome to the future of markup!</p>
+  	 </body>
+  	 </html>"      
   end
 
   def show
   	@article=Article.find(params[:id])
   end
-
+   
   
 end
